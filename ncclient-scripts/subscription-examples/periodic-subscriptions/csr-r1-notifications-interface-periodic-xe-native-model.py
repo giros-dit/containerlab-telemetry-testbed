c8620ab777c1a09e7831df0f1d3b294f34f6092a
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2023-04-17 00:16:11 +0200

    Updating ncclient scripts for YANG-Push subscriptions

---
+++ ncclient-scripts/subscription-examples/periodic-subscriptions/csr-r1-notifications-interface-periodic-xe-native-model.py
@@ -0,0 +1,41 @@
+from ncclient import manager
+from ncclient.xml_ import to_ele
+
+r1 = {
+    "host": "clab-telemetry-ixiac-lab-r1",
+    "port": 830,
+    "username": "admin",
+    "password": "admin",
+    "hostkey_verify": False,
+    "device_params": {"name": "csr"}
+}
+
+session = manager.connect(**r1)
+
+print ("Session ID: ", session.session_id)
+
+# When building the RPC request XML, use dampening-period for on-change notifications (when supported).
+# Otherwise, use period and specify an integer value.
+
+rpc = """
+
+    <establish-subscription xmlns="urn:ietf:params:xml:ns:yang:ietf-event-notifications"
+    xmlns:yp="urn:ietf:params:xml:ns:yang:ietf-yang-push">
+        <stream>yp:yang-push</stream>
+        <yp:xpath-filter>/interfaces-ios-xe-oper:interfaces/interface[name='GigabitEthernet2']/admin-status</yp:xpath-filter>
+        <yp:dampening-period>0</yp:dampening-period>
+    </establish-subscription>
+
+"""
+
+request = session.dispatch(to_ele(rpc))
+
+print(request)
+
+print("Notifications for device clab-telemetry-testbed-r1...")
+
+while True:
+    sub_data = session.take_notification(timeout=60)
+    if (sub_data != None):
+        print(sub_data.notification_xml)
+    print("------------------------")
