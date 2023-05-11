3baccbb87c7c7fda26b033abe2dbacfd83f5cf9a
David Martínez García <94689492+martinezgarciadavid@users.noreply.github.com>
2023-05-11 10:28:06 +0200

    Update destroy-testbed-lab.sh

--- destroy-testbed-lab.sh
+++ destroy-testbed-lab.sh
@@ -4,6 +4,9 @@ echo 'Destroying containerlab topology with Cisco IOS XE CSR1000v routers ...'
 
 sudo containerlab destroy --topo telemetry-testbed.yaml
 
+sudo rm .telemetry-testbed.yaml.bak
+sudo rm -Rf clab-telemetry-testbed/
+
 echo 'Done!'
 
 echo ''
